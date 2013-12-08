cb9e72b9aa21998d3485bab30afeaa1f2378061e
Punit <the.punit@gmail.com>
2013-12-07 21:27:51 -0800

    Integrated arduino getColor into MVC

--- web/arduino/getColor.php
+++ web/arduino/getColor.php
@@ -2,5 +2,8 @@
 // Colorlights project
 // Send contents of color datastore file; in special folder because .htaccess allows this to be directly accessed apart from 301 rewries
 
-include '../datastore/color.txt';
+require_once("controller.php");
+
+echo fetchColor();
+
 ?>
