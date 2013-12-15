4d853105d469fd1540b84a28d7bc098584eb7d79
Punit <the.punit@gmail.com>
2013-12-15 13:16:22 -0800

    Added cron task to run at every arduino color pull

--- web/arduino/getColor.php
+++ web/arduino/getColor.php
@@ -2,9 +2,13 @@
 // Colorlights project
 // Send contents of color datastore file; in special folder because .htaccess allows this to be directly accessed apart from 301 rewries
 
-require_once("../controller.php");
-chdir ( "../" );
+chdir("../");
+require_once("controller.php");
 
+// update latest data
+cron();
+
+// get data
 echo fetchColor();
 
 ?>
